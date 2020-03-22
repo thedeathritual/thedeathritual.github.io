e9bbc3ddb356434bba47f20b82da0da54909b46d
Sai Osore <sai.osore@gmail.com>
2020-03-23 01:17:18 +0200

    menu open bug fixed

--- js/main.js
+++ js/main.js
@@ -234,7 +234,7 @@ var mobileMenu = document.querySelector('.header__mobile-menu');
 wrapperMenu.addEventListener('click', function () {
   wrapperMenu.classList.toggle('header-mobile-btn--active');
   mobileMenu.classList.toggle('header__mobile-menu--active');
-  document.body.classList.toggle('body-fixed');
+  document.body.classList.toggle('overflow-hidden');
 });
 "use strict";
 
